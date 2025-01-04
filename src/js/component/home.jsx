245b65c80a295925a101c6d50dcec6c9aeb6fce0
Pablo Querales <pabloquerales20@gmail.com>
2025-01-04 12:30:54 +0100

    quitando al ringo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
@@ -15,9 +12,7 @@ const Home = () => {
 				If you see this green button... bootstrap is working...
 			</a>
 			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!
 			</p>
 		</div>
 	);
